c5aea06ffe8da8bfc62f62a36f5defc7247ef6e4
kyle beavin <kylebeavin@gmail.com>
2018-05-07 16:52:18 -0400

    URL var and http reqs kb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowseEntriesComponent } from './components/browse-entries/browse-entries.component';
 import { AddEntriesComponent } from './components/add-entries/add-entries.component';
+import { AuthService } from './services/auth.service';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+const Api_Url = 'http://localhost:3000/api/users';
+
 @Injectable()
 export class AuthService {
 
