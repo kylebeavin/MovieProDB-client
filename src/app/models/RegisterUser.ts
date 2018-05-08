55059ad57146238856ae18ad5abf04ac5d49f1ce
kyle beavin <kylebeavin@gmail.com>
2018-05-08 09:05:52 -0400

    added model kb

---
+++ src/app/models/RegisterUser.ts
@@ -0,0 +1,11 @@
+export interface RegisterUser {
+    firstName: string,
+    lastName: string,
+    email: string,
+    username: string,
+    password: string,
+    company: string,
+    title: string,
+    userType: number,
+    regStatus: boolean
+}
