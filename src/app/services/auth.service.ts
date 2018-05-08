6eb03d203225bc71e504da35ab35640206deda05
Brittany Hutter <35478645+bhutter512@users.noreply.github.com>
2018-05-08 10:51:11 -0400

    BH changed HTTP Client and imports

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/RegisterUser';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 const Api_Url = 'http://localhost:3000/api/users';
 
 @Injectable()
 export class AuthService {
 
-  constructor( private _http: Http) { }
+  constructor(private _http: HttpClient) { }
 
   register(regUserData: RegisterUser) {
     this._http.post(`${Api_Url}/register`, regUserData);
