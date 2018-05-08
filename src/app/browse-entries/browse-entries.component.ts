adec31d8fa62df2cd934f961a548ba0aa672a5ea
Brittany Hutter <35478645+bhutter512@users.noreply.github.com>
2018-05-08 10:16:34 -0400

    BH basic table functioning

--- src/app/browse-entries/browse-entries.component.ts
+++ src/app/browse-entries/browse-entries.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Entry } from './entry';
+import { ENTRIES } from './sample-entries';
+
 @Component({
   selector: 'app-browse-entries',
   templateUrl: './browse-entries.component.html',
   styleUrls: ['./browse-entries.component.scss']
 })
 export class BrowseEntriesComponent implements OnInit {
+  entries = ENTRIES;
+  displayedColumns = ["entryID", "sceneID", "productID", "creatorID", "creationDate", "editorID", "lastEdited"]
 
   constructor() { }
 
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatTableModule, MatInput } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule]
+  imports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule],
+  exports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule]
 })
 
 export class MaterialModule {};
