78ce6e3fca07221a8cde748aa53d01fc47297172
Brittany Hutter <35478645+bhutter512@users.noreply.github.com>
2018-05-09 16:10:15 -0400

    BH add movies form and service

--- src/app/components/add-movies/add-movies.component.ts
+++ src/app/components/add-movies/add-movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movies.service'
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-movies',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-movies.component.scss']
 })
 export class AddMoviesComponent implements OnInit {
+  private _movieForm: FormGroup;
 
-  constructor() { }
+  genres = [
+    {value: 'comedy-01', viewValue: 'Comedy'},
+    {value: 'action-02', viewValue: 'Action'},
+    {value: 'suspense-03', viewValue: 'Suspense'},
+    {value: 'horror-04', viewValue: 'Horror'},
+    {value: 'romance-05', viewValue: 'Romance'}
+  ]
+
+  constructor(private _form: FormBuilder, private _movieService: MovieService) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this._movieForm = this._form.group({
+      title: new FormControl,
+      productionCompany: new FormControl,
+      genre: new FormControl
+    })
+  }
+
+  onSubmit() {
+    this._movieService.addMovie(this._movieForm.value)
   }
 
 }
