0c6106d5be6ec863188fe5e76472235a0db6d6e4
kyle beavin <kylebeavin@gmail.com>
2018-05-09 09:21:23 -0400

    kb

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/RegisterUser';
 import { HttpClient } from '@angular/common/http';
+import { WSAEUSERS } from 'constants';
+import { userInfo } from 'os';
 
 const Api_Url = 'http://localhost:3000/api/users';
 
@@ -13,6 +15,9 @@ export class AuthService {
 
   register(regUserData: RegisterUser) {
     this._http.post(`${Api_Url}/register`, {'user': regUserData})
-      .subscribe(res => console.log('User created.'))
+      .subscribe(res => {
+        
+        localStorage.setItem('token', JSON.stringify(res.sessionToken))
+      })
   }
 }
