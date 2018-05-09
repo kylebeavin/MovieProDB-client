16fda29c995a7cab95977542793f3bab90ddc47b
kyle beavin <kylebeavin@gmail.com>
2018-05-09 16:03:54 -0400

    working on login session kb

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/RegisterUser';
+import { LoginUser } from '../models/LoginUser';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 const Api_Url = 'http://localhost:3000/api/users';
 
@@ -9,18 +11,24 @@ const Api_Url = 'http://localhost:3000/api/users';
 @Injectable()
 export class AuthService {
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient, private _router: Router) { }
 
   register(regUserData: RegisterUser) {
     this._http.post(`${Api_Url}/register`, {'user': regUserData})
       .subscribe(res => {
         const userData: any = res;
-        localStorage.setItem('sessionToken', userData.sessionToken)
+        localStorage.setItem('token', userData.sessionToken)
+        this._router.navigate(['/create'])
       })
   }
 
-  login() {
-
+  login(logUser: LoginUser) {
+    this._http.post(`${Api_Url}/login`, {'user': logUser})
+      .subscribe(res => {
+        const loginInfo: any = res;
+        localStorage.setItem('token', loginInfo.sessionToken)
+        this._router.navigate(['/create'])
+      })
   }
 
   logout() {
