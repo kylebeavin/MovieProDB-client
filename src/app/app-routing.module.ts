37902f8896f2a3c52827985cfa956c6b64c1bce2
nikayahale <nikayahale@outlook.com>
2018-05-10 11:46:31 -0400

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,40 +10,22 @@ import { AddMoviesComponent } from './components/add-movies/add-movies.component
 import { AddProductsComponent } from './components/add-products/add-products.component';
 import { Navbar2Component } from './components/navbar2/navbar2.component';
 import { SplashComponent } from './components/splash/splash.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
-  {
-    path: 'register',
-    component: RegisterComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'browse',
-    component: BrowseEntriesComponent
-  },
-  {
-    path: 'add-entries',
-    component: AddEntriesComponent
-  },
-  {
-    path: 'add-movies',
-    component: AddMoviesComponent
-  },
-  {
-    path: 'add-products',
-    component: AddProductsComponent
-  },
-  {
-    path: 'navbar2',
-    component: Navbar2Component
-  },
-  {
-    path: 'splash',
-    component: SplashComponent
-  }
+  { path: '', redirectTo: 'landing-page', pathMatch:'full'},
+  { path: 'add-entries', component: AddEntriesComponent },
+  { path: 'add-movies', component: AddMoviesComponent },
+  { path: 'add-products', component: AddProductsComponent },
+  { path: 'splash', component: SplashComponent, children: [
+    { path: 'navbar2', component: Navbar2Component },
+    { path: 'browse', component: BrowseEntriesComponent },
+  ] },
+  { path: 'landing-page', component: LandingPageComponent, children: [
+    { path: 'navbar', component: NavbarComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+  ]},
 ];
 
 @NgModule({
@@ -51,4 +33,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [RegisterComponent, LoginComponent, BrowseEntriesComponent, AddEntriesComponent, AddMoviesComponent, AddProductsComponent, SplashComponent, Navbar2Component]
+export const routingComponents = [RegisterComponent, LoginComponent, BrowseEntriesComponent, AddEntriesComponent, AddMoviesComponent, AddProductsComponent, SplashComponent, Navbar2Component, LandingPageComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { BrowseEntriesComponent } from './components/browse-entries/browse-entries.component';
-import { AddEntriesComponent } from './components/add-entries/add-entries.component';
-
 import { AuthService } from './services/auth.service';
 import { MovieService } from './services/movies.service';
 
@@ -16,20 +11,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { AddMoviesComponent } from './components/add-movies/add-movies.component';
-import { AddProductsComponent } from './components/add-products/add-products.component';
-import { Navbar2Component } from './components/navbar2/navbar2.component';
-import { SplashComponent } from './components/splash/splash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     routingComponents,
-    AddProductsComponent,
-    Navbar2Component,
-    SplashComponent,
-    AddMoviesComponent
   ],
   imports: [
     BrowserModule,
