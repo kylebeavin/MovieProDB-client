ddc63252876892d50eabca7804d6efd81601c068
kyle beavin <kylebeavin@gmail.com>
2018-05-10 16:57:41 -0400

    mat grid fun kb

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-
+  tiles = [
+    {text: 'One', cols: 1, rows: 1},
+    {text: 'Two', cols: 1, rows: 1},
+    {text: 'Three', cols: 1, rows: 1},
+    {text: 'Four', cols: 1, rows: 1}
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule } from '@angular/material';
+import { MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, 
+         MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, 
+         MatCardModule, MatGridListModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule],
-  exports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule]
+  imports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule,
+           MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule,
+           MatCardModule,MatGridListModule],
+  exports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule,
+           MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule,
+           MatCardModule,MatGridListModule]
 })
 
 export class MaterialModule {};
