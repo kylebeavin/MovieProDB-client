8280ecd981e55fbc4806a22a1b8abf17dc6a8726
Brittany Hutter <35478645+bhutter512@users.noreply.github.com>
2018-05-11 13:30:47 -0400

    BH fixing merges

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatGridListModule, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatGridListModule, MatCardModule, MatDialogModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatGridListModule, MatCardModule],
-  exports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatGridListModule, MatCardModule]
-=======
-import { MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatDialogModule } from '@angular/material';
-
-@NgModule({
-  imports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatDialogModule],
-  exports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatDialogModule]
->>>>>>> 02546186f5026580c668d26b294c90b16f4f16dd
+  imports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatGridListModule, MatCardModule, MatDialogModule],
+  exports: [MatButtonModule, MatTableModule, MatInputModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatGridListModule, MatCardModule, MatDialogModule]
 })
 
 export class MaterialModule {};
