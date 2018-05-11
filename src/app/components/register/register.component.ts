eb501d325c6104747e6e5032564e01d03d7e1063
kyle beavin <kylebeavin@gmail.com>
2018-05-11 14:27:51 -0400

    kb

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
   @Input() dontShow: boolean;
-
   private _registerForm: FormGroup;
 
   constructor(private _form: FormBuilder, private _authService: AuthService) { }
