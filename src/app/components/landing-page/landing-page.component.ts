7b1ac7bf396dda8d9903caf08dfcbbb286e33962
kyle beavin <kylebeavin@gmail.com>
2018-05-11 09:13:00 -0400

    working landing page grid kb

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -6,12 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-  tiles = [
-    {text: 'One', cols: 1, rows: 1},
-    {text: 'Two', cols: 1, rows: 1},
-    {text: 'Three', cols: 1, rows: 1},
-    {text: 'Four', cols: 1, rows: 1}
-  ];
   constructor() { }
 
   ngOnInit() {
